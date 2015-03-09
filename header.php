e42cdfad4deab5ac0295ba755593c0129a76040e
juscing <juscing@gmail.com>
2015-03-09 11:41:09 -0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme We aren't using it in favor of our own theme -->
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<!-- Our theme -->
+<link href="css/styles.css" rel="stylesheet">
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+<!-- Tweaks for this theme -->
+<script src="js/scripts.js"></script>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once('header.php');
+?>
+<title>Bootstrap 3 Template / Theme - Bootable</title>
+</head>
+<body>
+
+<?php
+require_once('footer.php');
+?>
