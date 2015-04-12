1565e7bebd77b2572eacd8580481004ad6dcc7b4
juscing <juscing@gmail.com>
2015-04-12 15:40:14 -0400

    more login stuff

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="container">
   	<div class="row">
-      <div class="col-md-12 text-right"><h5>©Company 2014</h5></div>
+      <div class="col-md-12 text-right"><h5>© 2015</h5></div>
     </div>
   </div>
 </footer>
