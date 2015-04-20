3ca9945a7e3f7f30179e78013c1ca03d8f75d836
Devon-Peroutky <devonperoutky@vividcortex.com>
2015-04-20 01:12:46 -0400

    Final first version of the rough draft

--- php/queryFull.php
+++ php/queryFull.php
@@ -5,14 +5,7 @@ require_once(ROOT_PATH . 'db/dbconnect.php');
 
 function queryDB($arguments) {
 	// Declarations
-	$movieMap = array(
-		$mTitle => array(
-			"data" => array(),
-			"actors" => array()
-		)
-	);
-
-	#$movieMap["Shrek"]["data"]["genre"] = "Comedy";
+	$movieMap = array();
 
 	//$db_connection = DbUtil::loginConnection();
 	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4750jci5kb', 'moviedbgroup', 'cs4750jci5kb');
@@ -21,32 +14,20 @@ function queryDB($arguments) {
 		return;
 	}
 
+	// Build the Query
 	$stmt = $db_connection->stmt_init();
 	$query = "SELECT * from (`Actor` NATURAL JOIN `StarredIn` NATURAL JOIN `Movie` NATURAL JOIN `Directed` NATURAL JOIN `Director`) WHERE";
-
 	foreach ($arguments as $key => $value) {
 		$query=$query . " `" . $key . "` LIKE '" . $value . "' AND ";
 	}
 	$query=substr($query,0, -5);
-	echo $query;
 
+	// Execute the Query
 	if ($stmt->prepare($query)) {
-
 		$stmt->execute();
 		$stmt->bind_result($director_id, $movie_id, $actor_id, $fname, $lname, $mtitle, $genreResponse, $uRating, $releaseYear, $runtime, $cRating, $plot, $dfname, $dlname);
 
-		echo "<table>";
-		echo "<tr><th>Title</th><th>Genre</th><th>User Rating</th><th>Year</th><th>Runtime</th><th>Critic Rating</th></tr>";
-
 		while($stmt->fetch()) {
-			echo "</table>";
-			echo '<div class="featurette" id="about">';
-	        echo '<img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">';
-	        echo '<h2 class="featurette-heading">'.$mtitle.'<span class="text-muted"></span></h2>';
-	        echo '<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>';
-	        echo '</div>';
-	        echo '<hr class="featurette-divider">';
-
 			$movieMap[$mtitle]["data"]["plot"] = $plot;
 	      	$movieMap[$mtitle]["data"]["director"] = $dfname." ".$dlname;
 			$movieMap[$mtitle]["data"]["cRating"] = $cRating;
@@ -56,8 +37,22 @@ function queryDB($arguments) {
 			$movieMap[$mtitle]["data"]["genre"] = $genreResponse;
 			$movieMap[$mtitle]["actors"][] = $fname." ".$lname;
     	}
-
-    	for()
+    	foreach($movieMap as $title => $movie) {
+			echo '<div class="featurette" id="about">';
+	        echo '<img style="height:500px; width:500px;" class="featurette-image img-circle img-responsive pull-right" src='. "img/movies/". str_replace(' ','',$title).'.jpg>';
+   	        //echo '<img class="featurette-image img-circle img-responsive pull-right" src="http://placehold.it/500x500">';
+	        echo '<h2 class="featurette-heading">'.$title.'<span class="text-muted"></span></h2>';
+	        echo '<p class="lead">'.$movieMap[$mtitle]["data"]["plot"].'</p>';
+	       	echo '<p class="lead">Director: '.$movieMap[$mtitle]["data"]["director"].'</p>';
+	        echo '<p class="lead">MetaCritic Rating: '.$movieMap[$mtitle]["data"]["cRating"].'</p>';
+	        echo '<p class="lead">Release: '.$movieMap[$mtitle]["data"]["releaseYear"].'</p>';
+	        echo '<p class="lead">User Ratings: '.$movieMap[$mtitle]["data"]["uRating"].'</p>';
+	        echo '<p class="lead">Genre: '.$movieMap[$mtitle]["data"]["genre"].'</p>';
+	        echo '<p class="lead">Actors: '.implode(', ', $movie["actors"]).'</p>';
+	        echo '</div>';
+	        echo '<hr class="featurette-divider">';
+    		echo "<br><br>";
+    	}
 	}
 }
 
@@ -74,9 +69,9 @@ $names = explode(" ", $actor, 2);
 $firstname = $names[0];
 
 if (sizeof($names)<2) {
-	$lastname=$firstname;
+	$lastname="";
 } else if ($names[1] =="") {
-	$lastname=$firstname;
+	$lastname="";
 } else {
 	$lastname=$names[1];
 }
@@ -107,11 +102,6 @@ if (strlen($director)>0) {
 	$params['director_first_name']= "%$director_firstname%";
 	$params['director_last_name']= "%$director_lastname%";
 } 
-
-// Validation
-foreach ($params as $key => $value) {
-	echo "Key: $key; Value: $value<br />\n";
-}
 
 queryDB($params);
 ?>
