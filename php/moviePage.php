b9ae19e539d8e0784fcc0b2a934b82903e6573e2
juscing <juscing@gmail.com>
2015-04-21 16:50:18 -0400

    Favoriting movies from movie page fully working

--- php/moviePage.php
+++ php/moviePage.php
@@ -6,12 +6,17 @@ require_once('../conf/config.php');
 require_once(ROOT_PATH . 'db/dbconnect.php');
 
 function queryDB($offset) {
+	$user = "";
+	if(isset($_SESSION['user'])) {
+		$user = $_SESSION['user'];
+	}
 	$db_connection = DbUtil::loginConnection();
 	$offset = intval($offset, $base = 10);
-	if ($stmt = $db_connection->prepare("SELECT movie_id, title, genre, year, runtime, user_rating, critic_rating FROM `Movie` LIMIT 20 OFFSET ?")) {
-		$stmt->bind_param("i", $offset);
+	$queryuser = "SELECT username, Movie.movie_id, title, genre, year, runtime, user_rating, critic_rating FROM `Movie` LEFT JOIN `Favorites` ON Movie.movie_id = Favorites.movie_id WHERE username = ? OR username IS NULL LIMIT 20 OFFSET ?";
+	if ($stmt = $db_connection->prepare($queryuser)) {
+		$stmt->bind_param("si", $user, $offset);
 		$stmt->execute();
-		$stmt->bind_result($id, $title, $genre, $year, $runtime, $user_rating, $critic_rating);
+		$stmt->bind_result($user, $id, $title, $genre, $year, $runtime, $user_rating, $critic_rating);
 		$count = 0;
 		if($offset == 0) {
 			echo '<div id="scroller"><h1>Movies</h1>';
@@ -41,7 +46,13 @@ function queryDB($offset) {
 			echo("<td>" . $user_rating . "</td>\n");
 			echo("<td>" . $critic_rating . "</td>\n");
 			if(isset($_SESSION['user'])) {
-				echo('<td><a href="favmovie.php?movie='.$id.'" class="star notfav"></a></td>');
+				echo('<td><a href="favmovie.php?movie='.$id.'" class="star ');
+				if(empty($user)) {
+					echo "notfav";				
+				} else {
+					echo "fav";				
+				}
+				echo '"></a></td>';
 				echo("<td>" . "Watch List" . "</td>\n");
 			}
 			echo "</tr>";
