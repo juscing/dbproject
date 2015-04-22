92e31650ef6e7a6ac6dd8d19cb8001981c5a19a5
juscing <juscing@gmail.com>
2015-04-22 03:25:06 -0400

    Moving stuff into index js

--- index.php
+++ index.php
@@ -170,6 +170,74 @@ require_once(ROOT_PATH . 'db/dbconnect.php');
   });
 */
 
+$('#actorDiv span').click(function() {
+
+ 	var actorName = $(this).text();
+ 	console.log(actorName);
+
+    var res = $("#results");
+    res.fadeOut(function () {
+        res.empty();
+        res.load("php/queryResults.php", { 
+            actor: actorName,
+            phrase: "Here are all the movies starring " + actorName + "...",
+        }, function () {
+            res.fadeIn();
+        });
+    });
+});
+
+$("#results").on("click", '#genreDiv span', function() {
+
+ 	var genreType = $(this).text();
+ 	console.log(genreType);
+
+    var res = $("#results");
+    res.fadeOut(function () {
+        res.empty();
+        res.load("php/queryResults.php", { 
+            genre: genreType,
+            phrase: genreType,
+        }, function () {
+            res.fadeIn();
+        });
+    });
+});
+
+$("#results").on("click", '#directorDiv span', function() {
+
+ 	var directorName = $(this).text();
+ 	console.log(directorName);
+
+    var res = $("#results");
+    res.fadeOut(function () {
+        res.empty();
+        res.load("php/queryResults.php", { 
+            director: directorName,
+            phrase: "Here are all the movies directed by " + directorName + "...",
+        }, function () {
+            res.fadeIn();
+        });
+    });
+});
+
+$("#results").on("click", '#yearDiv span', function() {
+
+ 	var yearNumber = $(this).text();
+ 	console.log(yearNumber);
+
+    var res = $("#results");
+    res.fadeOut(function () {
+        res.empty();
+        res.load("php/queryResults.php", { 
+            year: yearNumber,
+            phrase: "Here are all the movies from " + yearNumber + "...",
+        }, function () {
+            res.fadeIn();
+        });
+    });
+});
+
 
 });
 function scroller(item) {
